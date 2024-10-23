9f34dbd4ce08c7c24966778bbaec8902ea3f5c9a
Jorjich18 <makoson355@gmail.com>
2024-10-23 13:20:51 +0000

    Version 1

--- .ccls-cache/@home@runner@Samostiyna4Iashchuk/main.cpp
+++ .ccls-cache/@home@runner@Samostiyna4Iashchuk/main.cpp
@@ -1,3 +1,18 @@
-#include <iostream>
+#include <iostream> //include file (default)
+using namespace std; //using a namespace (example: std)
 
-int main() { std::cout << "Hello World!\n"; }
+//The main program
+int main()
+{
+  double A[8]; //The array of 8 numbers
+  A[0] = 1;
+  A[1] = 2;
+  A[2] = 3;
+  A[3] = 4;
+  A[4] = 5;
+  A[5] = 6;
+  A[6] = 7;
+  A[7] = 8;
+  cout << A[0] << "\t" << A[1] << "\t" << A[2] << "\t" << A[3] << "\t" << A[4] << "\t" << A[5] << "\t" << A[6] << "\t" << A[7] << endl; //Printing the array
+  return 0;
+}
